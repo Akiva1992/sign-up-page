dd02e3f833e965dfaede1a015605f787bfc5652c
Akiva1992 <breuerakiva@gmail.com>
2022-10-04 20:51:51 +0300

    finished styling form still need to work on js pwd validation and sunmit btn style and phone responsive

---
+++ app.js
@@ -0,0 +1,12 @@
+// const password = document.getElementById("password");
+// const confirmPassword = document.getElementById("confirm-password");
+
+
+// confirmPassword.addEventListener("input", (event) => {
+//   if (password!=confirmPassword) {
+//     password.setCustomValidity("The passwords don't match");
+//     password.reportValidity();
+//   } else {
+//     password.setCustomValidity("");
+//   }
+// });
